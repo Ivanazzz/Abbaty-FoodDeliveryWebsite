34f8b21bf6844bd089edf1c54d94d0a1b2b3a3d2
DESKTOP-HHPDH9M\Ivana.Boyukliyska <iva1100b@gmail.com>
2024-02-09 14:30:26 +0200

    Add UnitTest (empty - not working)

---
+++ FoodDeliveryWebsite/FoodDeliveryWebsiteUnitTests/Addresses/AddressTests/AddressAddTest.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using FoodDeliveryWebsite.Controllers;
+using FoodDeliveryWebsite.Models.Dtos;
+using FoodDeliveryWebsite.Models.Entities;
+using FoodDeliveryWebsite.Models;
+using FoodDeliveryWebsite.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDeliveryWebsite.UnitTests.Addresses.AddressTests
+{
+    public class AddressAddTest
+    {
+        private readonly Mock<IAddressRepository> mockRepo;
+        private readonly Mock<FoodDeliveryWebsiteDbContext> mockContext;
+        private readonly Mock<IMapper> mockMapper;
+
+        public AddressAddTest()
+        {
+            mockRepo = new Mock<IAddressRepository>();
+            mockContext = new Mock<FoodDeliveryWebsiteDbContext>();
+            mockMapper = new Mock<IMapper>();
+        }
+
+        [Fact]
+        public async Task GetAddressesAsync_ReturnsExpectedAddresses()
+        {
+            // Arrange
+            //var userEmail = "test@example.com";
+            //var expectedAddresses = AddressTestData.GetTestAddresses();
+
+            
+
+            // Act
+            //var result = await addressRepository.GetAddressesAsync(userEmail);
+
+            // Assert
+            //Assert.Equal(expectedAddresses.Count, result.Count);
+        }
+    }
+}
