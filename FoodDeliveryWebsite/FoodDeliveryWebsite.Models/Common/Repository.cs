2f0148526c4d1091783c55cf4de963eb0b92c790
Ivana <iva1100b@gmail.com>
2024-03-11 19:53:43 +0200

    Server: update tha IRepository and Repository

--- FoodDeliveryWebsite/FoodDeliveryWebsite.Models/Common/Repository.cs
+++ FoodDeliveryWebsite/FoodDeliveryWebsite.Models/Common/Repository.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Linq.Expressions;
 
 namespace FoodDeliveryWebsite.Models.Common
 {
+    /// <summary>
+    /// Implementation of repository access methods
+    /// for Relational Database Engine
+    /// </summary>
+    /// <typeparam name="T">Type of the data table to which 
+    /// current reposity is attached</typeparam>
     public class Repository : IRepository
     {
+        /// <summary>
+        /// Entity framework DB context holding connection information and properties
+        /// and tracking entity states 
+        /// </summary>
         private readonly DbContext context;
 
         public Repository(FoodDeliveryWebsiteDbContext _context)
@@ -11,32 +23,163 @@ namespace FoodDeliveryWebsite.Models.Common
             context = _context;
         }
 
+        /// <summary>
+        /// Representation of table in database
+        /// </summary>
         private DbSet<T> DbSet<T>() where T : class
         {
-            // returns the table from the database
             return context.Set<T>();
         }
 
-        public IQueryable<T> All<T>() where T : class
-        {
-            return DbSet<T>();
-        }
-
-        public IQueryable<T> AllReadOnly<T>() where T : class
-        {
-            return DbSet<T>()
-                .AsNoTracking();
-        }
-
+        /// <summary>
+        /// Adds entity to the database
+        /// </summary>
+        /// <param name="entity">Entity to add</param>
         public async Task AddAsync<T>(T entity) where T : class
         {
             await DbSet<T>()
                 .AddAsync(entity);
         }
 
+        /// <summary>
+        /// Ads collection of entities to the database
+        /// </summary>
+        /// <param name="entities">Enumerable list of entities</param>
+        public async Task AddRangeAsync<T>(IEnumerable<T> entities) where T : class
+        {
+            await DbSet<T>()
+                .AddRangeAsync(entities);
+        }
+
+        /// <summary>
+        /// All records in a table
+        /// </summary>
+        /// <returns>Queryable expression tree</returns>
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsQueryable();
+        }
+
+        public IQueryable<T> All<T>(Expression<Func<T, bool>> search) where T : class
+        {
+            return this.DbSet<T>()
+                .Where(search);
+        }
+
+        /// <summary>
+        /// The result collection won't be tracked by the context
+        /// </summary>
+        /// <returns>Expression tree</returns>
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return this.DbSet<T>()
+                .AsNoTracking();
+        }
+
+        public IQueryable<T> AllReadonly<T>(Expression<Func<T, bool>> search) where T : class
+        {
+            return this.DbSet<T>()
+                .Where(search)
+                .AsNoTracking();
+        }
+
+        /// <summary>
+        /// Deletes a record from database
+        /// </summary>
+        /// <param name="entity">Entity representing record to be deleted</param>
+        public void Delete<T>(T entity) where T : class
+        {
+            EntityEntry entry = context.Entry(entity);
+
+            if (entry.State == EntityState.Detached)
+            {
+                this.DbSet<T>().Attach(entity);
+            }
+
+            entry.State = EntityState.Deleted;
+        }
+
+        /// <summary>
+        /// Deletes a record from database
+        /// </summary>
+        /// <param name="id">Identificator of record to be deleted</param>
+        public async Task DeleteAsync<T>(object id) where T : class
+        {
+            T entity = await GetByIdAsync<T>(id);
+
+            Delete<T>(entity);
+        }
+
+        public void DeleteRange<T>(IEnumerable<T> entities) where T : class
+        {
+            this.DbSet<T>()
+                .RemoveRange(entities);
+        }
+
+        public void DeleteRange<T>(Expression<Func<T, bool>> deleteWhereClause) where T : class
+        {
+            var entities = All<T>(deleteWhereClause);
+
+            DeleteRange(entities);
+        }
+
+        /// <summary>
+        /// Detaches given entity from the context
+        /// </summary>
+        /// <param name="entity">Entity to be detached</param>
+        public void Detach<T>(T entity) where T : class
+        {
+            EntityEntry entry = context.Entry(entity);
+
+            entry.State = EntityState.Detached;
+        }
+
+        /// <summary>
+        /// Gets specific record from database by primary key
+        /// </summary>
+        /// <param name="id">record identificator</param>
+        /// <returns>Single record</returns>
+        public async Task<T> GetByIdAsync<T>(object id) where T : class
+        {
+            return await DbSet<T>()
+                .FindAsync(id);
+        }
+
+        public async Task<T> GetByIdsAsync<T>(object[] id) where T : class
+        {
+            return await DbSet<T>()
+                .FindAsync(id);
+        }
+
+        /// <summary>
+        /// Saves all made changes in trasaction
+        /// </summary>
+        /// <returns>Error code</returns>
         public async Task<int> SaveChangesAsync()
         {
-            return await context.SaveChangesAsync();
+            return await context
+                .SaveChangesAsync();
+        }
+
+        /// <summary>
+        /// Updates a record in database
+        /// </summary>
+        /// <param name="entity">Entity for record to be updated</param>
+        public void Update<T>(T entity) where T : class
+        {
+            this.DbSet<T>()
+                .Update(entity);
+        }
+
+        /// <summary>
+        /// Updates set of records in the database
+        /// </summary>
+        /// <param name="entities">Enumerable collection of entities to be updated</param>
+        public void UpdateRange<T>(IEnumerable<T> entities) where T : class
+        {
+            this.DbSet<T>()
+                .UpdateRange(entities);
         }
     }
 }
